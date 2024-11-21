6db0791b6093dba3017c6a9a0529db60999a2193
Mani-TheDeveloper <manishankar.ams@gmail.com>
2024-11-21 12:46:31 +0530

    Update App.java

--- Library Management System/src/App.java
+++ Library Management System/src/App.java
@@ -2,17 +2,10 @@
 import com.library.management.db.LibraryDB;
 import com.library.management.gui.Gui;
 
-
 public class App {
 
     private final LibraryDB libraryDB = new LibraryDB();
 
-    /*
-     * 5465
-     * Java
-     * BalaGuruSwamy
-     * CSE
-     */
     public App() {
         libraryDB.assignConnection();
         @SuppressWarnings("unused")
